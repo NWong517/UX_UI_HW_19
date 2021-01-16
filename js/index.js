cc82db07de208c2c1a8b0e4c0502bd1a21bf9f94
NWong517 <NWong517@users.noreply.github.com>
2021-01-15 21:38:05 -0800

    Interactive Element

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$("#panel").slideUp();
+$("#flip").click(function(){
+    $("#panel").slideToggle();
+});     
+
+$("#panel2").slideUp();
+$("#flip2").click(function(){
+    $("#panel2").slideToggle();
+});     
+
+$("#panel3").slideUp();
+$("#flip3").click(function(){
+     $("#panel3").slideToggle();
+});     
